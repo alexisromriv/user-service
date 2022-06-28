7135f1a327928c41eb5d88039f549627408c8538
Alexis Romero <dev.alexisromero@gmail.com>
2022-06-27 22:31:21 -0300

    PHPUnit Setup

--- src/app/User/Application/UserSignup.php
+++ src/app/User/Application/UserSignup.php
@@ -1,6 +1,11 @@
 <?php 
 
+namespace MyApp\User\Application;
+
 class UserSignup
 {
-    
+    public function sum($num1, $num2)
+    {
+        return $num1 + $num2;
+    }
 }
---
+++ src/tests/User/Application/UserSignupTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use MyApp\User\Application\UserSignup;
+use PHPUnit\Framework\TestCase;
+
+class UserSignupTest extends TestCase
+{
+
+    public function testMyTest(): void
+    {
+        $service = new UserSignup();
+        self::assertEquals($service->sum(1,1), 2);
+    }
+}
